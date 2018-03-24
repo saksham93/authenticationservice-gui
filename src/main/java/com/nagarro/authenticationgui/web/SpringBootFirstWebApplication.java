821508aa0095c8f87d5b590a6658b22d7a15a943
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-03-25 03:29:04 +0530

    Updated

--- src/main/java/com/nagarro/authenticationgui/web/SpringBootFirstWebApplication.java
+++ src/main/java/com/nagarro/authenticationgui/web/SpringBootFirstWebApplication.java
@@ -2,8 +2,13 @@ package com.nagarro.authenticationgui.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan("com.nagarro.authenticationgui.web.*")
 public class SpringBootFirstWebApplication {
@@ -11,5 +16,10 @@ public class SpringBootFirstWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirstWebApplication.class, args);
 	}
+	
+	@Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
 }
--- src/main/java/com/nagarro/authenticationgui/web/controller/LoginController.java
+++ src/main/java/com/nagarro/authenticationgui/web/controller/LoginController.java
@@ -1,30 +1,45 @@
 package com.nagarro.authenticationgui.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 
 @Controller
 @SessionAttributes("name")
 public class LoginController {
 
-	@RequestMapping(value="/login", method = RequestMethod.GET)
-    public String showLoginPage(ModelMap model){
-        return "login";
-    }
-	
-    @RequestMapping(value="/login", method = RequestMethod.POST)
-    public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password){
-		// boolean isValidUser = service.validateUser(name, password);
-		// if (!isValidUser) {
-		// model.put("errorMessage", "Invalid Credentials");
-		// return "login";
-		// }
-        model.put("name", name);
-        model.put("password", password);
-        return "welcome";
-    }
+	@Autowired
+	private RestTemplate restTemplate;
+	@Autowired
+	private EurekaClient eurekaClient;
+
+	@Value("${service.user.authentication}")
+	private String userAuthenticationServiceId;
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model) {
+		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password) {
+		Application application = eurekaClient.getApplication(userAuthenticationServiceId);
+		InstanceInfo instanceInfo = application.getInstances().get(0);
+		String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/";
+		System.out.println("URL" + url);
+		Integer count = restTemplate.getForObject(url, Integer.class);
+		model.put("name", count);
+		model.put("password", password);
+		return "welcome";
+	}
 }
