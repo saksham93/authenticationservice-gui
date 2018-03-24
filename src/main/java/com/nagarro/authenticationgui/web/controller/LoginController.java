9d42e433f8be7f641e2b7d18cf93ec8e13ed0a72
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-03-25 01:28:40 +0530

    Updated

--- src/main/java/com/nagarro/authenticationgui/web/controller/LoginController.java
+++ src/main/java/com/nagarro/authenticationgui/web/controller/LoginController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-@Controller
+
+F@Controller
 @SessionAttributes("name")
 public class LoginController {
 
