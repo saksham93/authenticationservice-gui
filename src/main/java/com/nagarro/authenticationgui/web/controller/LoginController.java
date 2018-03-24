34af9116d887d0f28a411e484121eb041ba3a1d0
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-03-25 01:31:04 +0530

    Bug fix

--- src/main/java/com/nagarro/authenticationgui/web/controller/LoginController.java
+++ src/main/java/com/nagarro/authenticationgui/web/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-F@Controller
+@Controller
 @SessionAttributes("name")
 public class LoginController {
 
