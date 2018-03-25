ca1702a04de9321fe2764c82443d3503b671dabe
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-03-25 22:12:22 +0530

    Úpdated

--- src/main/java/com/nagarro/authenticationgui/web/controller/LoginController.java
+++ src/main/java/com/nagarro/authenticationgui/web/controller/LoginController.java
@@ -1,13 +1,14 @@
 package com.nagarro.authenticationgui.web.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.client.RestTemplate;
@@ -33,14 +34,21 @@ public class LoginController {
 		return "login";
 	}
 
-	@PostMapping(value="/login")
+	@PostMapping(value = "/login")
 	public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password) {
 		Application application = eurekaClient.getApplication(userAuthenticationServiceId);
 		InstanceInfo instanceInfo = application.getInstances().get(0);
+		Map<String, String> vars = new HashMap<>();
+		vars.put("uid", name);
+		vars.put("password", password);
 		String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/";
 		System.out.println("URL" + url);
-		Integer count = restTemplate.getForObject(url, Integer.class);
-		model.put("name", count);
+		Boolean isPresent = restTemplate.postForObject(url + "{uid}/{password}", "", Boolean.class, vars);
+		if (isPresent) {
+			model.put("name", "Authenticated");
+		} else {
+			model.put("name", "Invalid Credentials");
+		}
 		model.put("password", password);
 		return "welcome";
 	}
